d5122bfb9bbaf395c82a2fc936d1cca9649f4ee2
Soorya Sankar <soorya.s@secureforme.io>
2025-10-08 19:34:38 +0530

    jwt added

---
+++ MicroService/SharedResources/DependencyInjection/JWTAuthenticationScheme.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace SharedResources.DependencyInjection
+{
+    public static class JWTAuthenticationScheme
+    {
+
+        public static IServiceCollection AddJWTAuthenticationScheme(this IServiceCollection services, IConfiguration config)
+        {
+            //add JWT service
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer("Bearer", opt =>
+            {
+                var key = Encoding.UTF8.GetBytes(config.GetSection("Authentication:Key").Value!);
+                string issuer = config.GetSection("Authentication:Issuer").Value!;
+                string audience = config.GetSection("Authentication:Audience").Value!;
+
+
+                opt.RequireHttpsMetadata = false;
+                opt.SaveToken = true;
+                opt.TokenValidationParameters = new TokenValidationParameters
+                {
+
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = false,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = issuer,
+                    ValidAudience = audience,
+                    IssuerSigningKey = new SymmetricSecurityKey(key)
+                };
+
+            });
+            return services;
+        }
+    }
+}
---
+++ MicroService/SharedResources/Middleware/ListenToOnlyApiGateway.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace SharedResources.Middleware
+{
+    public class ListenToOnlyApiGateway(RequestDelegate next)
+    {
+        public async Task InvokeAsync(HttpContext context)
+        {
+            //Extract specific header from the request 
+
+            var signedHeader = context.Request.Headers["Api-Gateway"];
+
+            //NULL means ,the request is not coming from the Api Gateway
+
+            if (signedHeader.FirstOrDefault() is null)
+            {
+                context.Response.StatusCode = StatusCodes.Status503ServiceUnavailable;
+                await context.Response.WriteAsync("Sorry, service is unavailable");
+                return;
+            }
+            else 
+            {
+                await next(context);
+            }
+        }
+    }
+}
